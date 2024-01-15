4bcfb31e6a0017eb99d576d92a428dedf77e5612
dand <79340018+dZyad@users.noreply.github.com>
2024-01-15 10:33:14 -0300

    Add button to resize the display

--- board.js
+++ board.js
@@ -2,7 +2,10 @@ const display = document.querySelector('#display');
 
 createTable(16);
 
+const resizeButton = document.querySelector('#resize');
 let cells = document.querySelectorAll('.column');
+
+resizeButton.addEventListener('click', resizeDisplay);
 
 function createTable(dim) {
     for ( let i = 0 ; i < dim ; i++ ) {
@@ -17,12 +20,41 @@ function createTable(dim) {
     }
 }
 
-cells.forEach(cell => {
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'black';
-    });
+addPixelBehavior();
 
-    cell.addEventListener('mouseleave', () => {
-        cell.style.backgroundColor = 'grey';
+const isValidNumber = (value) => {
+    return value > 0 && value < 101;
+}
+const newUserDisplayValue = () => {
+    let newValue = +prompt("Insert a new value for the display. From 1 to 100.", "24");
+    while( !isValidNumber(newValue) ) {
+        newValue = +prompt("Insert a valid value from 1 to 100.", "24");
+    }
+    return newValue;
+}
+
+function addPixelBehavior() {
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        });
+
+        cell.addEventListener('mouseleave', () => {
+            cell.style.backgroundColor = 'grey';
+        });
     });
-});
+}
+
+function clearDisplay() {
+    while( display.lastElementChild ) {
+        display.removeChild(display.lastElementChild);
+    }
+}
+
+function resizeDisplay() {
+    const newValue = newUserDisplayValue();
+    clearDisplay();
+    createTable(newValue);
+    cells = document.querySelectorAll('.column');
+    addPixelBehavior();
+}
